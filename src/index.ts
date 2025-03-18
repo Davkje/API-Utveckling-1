032049270b22f689052eba8c41511e01f2f4b559
David Kjellstrand <david.kjellstrand@gmail.com>
2025-03-18 11:40:12 +0100

    feat: add todo and post classes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { Todo } from './models/Todos';
+import { Post } from './models/Posts';
 const app = express();
+const PORT = 3000;
 
+// Define root path with message
+// req, handles the incoming request from the client
+// res, handles the outgoing response back ti the client
+// May use _ to indicate that the parameter is not used
+app.get('/', (_:Request, res: Response) => {
+  res.send('Hello World!');
+});
 
-const PORT = 3000
+const todos: Todo[] = [
+  new Todo('Handla'),
+  new Todo('Chilla'),
+  new Todo('Jobba'),
+  new Todo('Festa'),
+]
+
+app.get('/todos', (_: Request, res: Response) => {
+  res.json(todos);
+})
+
+const posts: Post[] = [
+  new Post('David',),
+  new Post('Josefin'),
+  new Post('Sascha'),
+]
+
+app.get('/posts', (_: Request, res: Response) => {
+  res.json(posts);
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
 })
